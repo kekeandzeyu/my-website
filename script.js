6c8e6e107bb5bb83ad8f56b5fc00a3af02c9b87e
kekeandzeyu <kekeandzeyu@gmail.com>
2024-09-13 08:51:57 +0800

    change outlook

--- script.js
+++ script.js
@@ -66,3 +66,41 @@ languageLinks.forEach(link => {
         dropdownContent.style.display = 'none';
     });
 });
+
+const userOs = document.querySelector(".os");
+
+let os = "unknown";
+
+function getOS() {
+    const userAgent = window.navigator.userAgent;
+    const platform = window.navigator.userAgentData?.platform || window.navigator.platform;
+    const macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"];
+    const windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"];
+    const iosPlatforms = ["iPhone", "iPad", "iPod"];
+
+    if (macosPlatforms.includes(platform)) {
+        os = "Mac OS";
+    } else if (iosPlatforms.includes(platform)) {
+        os = "iOS";
+    } else if (windowsPlatforms.includes(platform)) {
+        os = "Windows";
+    } else if (/Android/.test(userAgent)) {
+        os = "Android";
+    } else if (/Linux/.test(platform)) {
+        os = "Linux";
+    }
+
+    return os;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    getOS();
+
+    if (os === "iOS") {
+        document.body.style.fontFamily = 'SF Pro';
+    } else {
+        document.body.style.fontFamily = 'Google Sans';
+    }
+
+    userOs.innerText = `${os}`;
+});

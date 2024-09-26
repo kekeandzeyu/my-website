5948cd68739b85c1816a8b340a70c2a0ffd25a34
kekeandzeyu <kekeandzeyu@gmail.com>
2024-09-26 16:31:44 +0800

    change outlook

--- script.js
+++ script.js
@@ -72,36 +72,36 @@ languageLinks.forEach(link => {
     });
 });
 
-function getOS() {
-    const userAgent = window.navigator.userAgent;
-    const platform = window.navigator.userAgentData?.platform || window.navigator.platform;
-    const macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"];
-    const windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"];
-    const iosPlatforms = ["iPhone", "iPad", "iPod"];
-
-    if (macosPlatforms.includes(platform)) {
-        os = "Mac OS";
-    } else if (iosPlatforms.includes(platform)) {
-        os = "iOS";
-    } else if (windowsPlatforms.includes(platform)) {
-        os = "Windows";
-    } else if (/Android/.test(userAgent)) {
-        os = "Android";
-    } else if (/Linux/.test(platform)) {
-        os = "Linux";
-    }
-
-    return os;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    getOS();
-
-    if (os === "iOS") {
-        document.body.style.fontFamily = 'Google Sans Regular';
-    } else {
-        document.body.style.fontFamily = 'Google Sans';
-    }
-
-    userOs.innerText = `${os}`;
-});
+// function getOS() {
+//     const userAgent = window.navigator.userAgent;
+//     const platform = window.navigator.userAgentData?.platform || window.navigator.platform;
+//     const macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"];
+//     const windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"];
+//     const iosPlatforms = ["iPhone", "iPad", "iPod"];
+//
+//     if (macosPlatforms.includes(platform)) {
+//         os = "Mac OS";
+//     } else if (iosPlatforms.includes(platform)) {
+//         os = "iOS";
+//     } else if (windowsPlatforms.includes(platform)) {
+//         os = "Windows";
+//     } else if (/Android/.test(userAgent)) {
+//         os = "Android";
+//     } else if (/Linux/.test(platform)) {
+//         os = "Linux";
+//     }
+//
+//     return os;
+// }
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//     getOS();
+//
+//     if (os === "iOS") {
+//         document.body.style.fontFamily = 'Google Sans Regular';
+//     } else {
+//         document.body.style.fontFamily = 'Google Sans';
+//     }
+//
+//     userOs.innerText = `${os}`;
+// });
